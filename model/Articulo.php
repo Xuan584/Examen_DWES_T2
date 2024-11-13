d8558aece1a0e2eaa3c8b538ba560e2f5212a8a3
Xuan584 <xuangarciaro@gmail.com>
2024-11-13 08:54:40 +0100

    Clases Articulo y Bebida

--- model/Articulo.php
+++ model/Articulo.php
@@ -1,2 +1,65 @@
 <?php
 // Clase Artículo
+class Articulo{
+    public $nombre;
+    public $precio;
+    public $disponibilidad;
+    public $categoría;
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoría){
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponibilidad = $disponibilidad;
+        $this->categoría = $categoría;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function getDisponibilidad()
+    {
+        return $this->disponibilidad;
+    }
+
+    public function setDisponibilidad($disponibilidad)
+    {
+        $this->disponibilidad = $disponibilidad;
+
+        return $this;
+    }
+
+    public function getCategoría()
+    {
+        return $this->categoría;
+    }
+
+    public function setCategoría($categoría)
+    {
+        $this->categoría = $categoría;
+
+        return $this;
+    }
+
+
+}
--- model/Bebida.php
+++ model/Bebida.php
@@ -1,2 +1,37 @@
 <?php
 // Clase Bebida que hereda de Artículo
+
+class Bebida extends Articulo{
+    public $tamaño;
+    public $temperatura;
+
+    public function __construct($nombre, $precio, $disponibilidad, $categoría, $tamaño, $temperatura){
+        parent::__construct($nombre,$precio,$disponibilidad,$categoría);
+        $this->tamaño = $tamaño;
+        $this->temperatura = $temperatura;
+    }
+ 
+    public function getTamaño()
+    {
+        return $this->tamaño;
+    }
+
+    public function setTamaño($tamaño)
+    {
+        $this->tamaño = $tamaño;
+
+        return $this;
+    }
+
+    public function getTemperatura()
+    {
+        return $this->temperatura;
+    }
+
+    public function setTemperatura($temperatura)
+    {
+        $this->temperatura = $temperatura;
+
+        return $this;
+    }
+}
